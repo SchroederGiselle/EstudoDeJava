28911e5da460e17ed1b453d28ef1a53145aa5101
SchroederGiselle <giselleschroeder05@gmail.com>
2024-09-21 11:33:51 -0300

    EstruturasDeRepetição-For

---
+++ Projetos/src/ExemploFor.java
@@ -0,0 +1,19 @@
+public class ExemploFor {
+    public static void main(String[] args) throws Exception {
+        
+        /* for (bloco de inicialização(váriavel); expressão booleana de validação; bloco de atualização e atualização){
+
+            //comando que será executado até que a expressão de validação torne-se falsa
+        } */
+
+    for(int carneirinhos = 1; carneirinhos  <=20; carneirinhos ++){
+
+        System.out.println(carneirinhos + " Carneirinhos ");
+
+    }
+    
+    System.out.println("Giselle Dormiu");
+
+
+    }
+}
---
+++ Projetos/src/ExemploForArray.java
@@ -0,0 +1,21 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+        String pokemons [] = {"Pikachu" , "Boubasour" , "Charmander"};
+        
+        for (int x = 0; x < pokemons.length; x++){
+            System.out.println("O Pokemons no indice x = " + x + " é " + pokemons [x]);
+        }
+        
+    }
+    
+    public static void arrayListMelhorado(String[] args) {
+        
+        String Winksx [] = {"Estella" , "Flora" , "Blue"};
+    
+            for (String Winks : Winksx){
+                System.out.println("A Winks no indice é:" + Winks);
+            }
+ }
+}
+
